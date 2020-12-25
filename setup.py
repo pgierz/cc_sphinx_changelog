43a8501eac0f07fedbfb59f5443f7f7feef8bfd2
Paul Gierz <pgierz@awi.de>
2020-12-25 16:45:39 +0100

    fix(setup.py): adds missing dependencies
    Closes #4

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open('README.rst') as readme_file:
 with open('CHANGELOG.md') as history_file:
     history = history_file.read()
 
-requirements = ["gitpython", "backpedal"]
+requirements = ["gitpython", "backpedal", "ansiwrap", "crayons"]
 
 setup_requirements = [ ]
 
