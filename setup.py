afc76c2b1175609f5888bd7df4ae589cf739f4ec
Paul Gierz <pgierz@awi.de>
2020-12-28 11:42:25 +0100

    fix(setup.py): adds missing dependencies

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open('README.rst') as readme_file:
 with open('CHANGELOG.md') as history_file:
     history = history_file.read()
 
-requirements = ["gitpython", "backpedal", "ansiwrap", "crayons"]
+requirements = ["gitpython", "backpedal", "ansiwrap", "crayons", "docutils", "sphinx"]
 
 setup_requirements = [ ]
 
@@ -25,13 +25,10 @@ setup(
         'Intended Audience :: Developers',
         'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
         'Natural Language :: English',
-        "Programming Language :: Python :: 2",
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
     ],
     description="Automatically generates a releases-style changelog based on conventional commit messages.",
     install_requires=requirements,
